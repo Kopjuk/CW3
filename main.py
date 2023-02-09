961955bd4e1dbb8bbed91a336086ddc022362e08
Kopjuk <kopjuk42rus@gmail.com>
2023-02-09 12:38:14 +0700

    work day 2 add Readme, requirements

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from utils import get_data, get_filtered_data
+from utils import get_data, get_filtered_data, get_lats_values, get_formatted_data
 
 
 def main():
     OPERATION_URL = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230208%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230208T133852Z&X-Amz-Expires=86400&X-Amz-Signature=8446a34397803a027ad271b9f26863fbd6542da89eec97c337bdaed44d1dc7ea&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22operations.json%22&x-id=GetObject"
     FILTERED_EMPTY_FROM = True
+    COUNT_LAST_VALUES = 5
 
     data, info = get_data(OPERATION_URL)
     if not data:
@@ -12,7 +13,12 @@ def main():
         print(info)
 
     data = get_filtered_data(data, filtered_empty_from=FILTERED_EMPTY_FROM)
+    data = get_lats_values(data, COUNT_LAST_VALUES)
+    data = get_formatted_data(data)
 
+    print("INFO: Вывод дынных:")
+    for row in data:
+        print(row, end='\n\n')
 
 if __name__ == '__main__':
     main()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 import pytest
 
-
-from utils import get_data
+from utils import get_data, get_filtered_data, get_lats_values, get_formatted_data
 
 
 def test_get_date(test_url):
@@ -17,5 +16,27 @@ def test_get_date(test_url):
     assert get_data("https://github.com/Kopjuk/test_develop")[0] is None
     assert get_data("https://github.com/Kopjuk/test_develope")[0] is None
 
-def test_get_filtered_data():
-    get_filtered_data
+
+def test_get_filtered_data(test_data):
+    """
+    Проверяем филтрацию
+    1 количество данных = 5
+    2 количество отфильтрованых данных с отправителем = 4
+    """
+    assert len(get_filtered_data(test_data)) == 4
+    assert len(get_filtered_data(test_data, filtered_empty_from=True)) == 3
+
+
+def test_get_lats_values(test_data):
+    """
+    Проверяем сортировку по дате
+    """
+    data = get_lats_values(test_data, 4)
+    assert data[0]["date"] == '2020-07-03T18:35:29.512364'
+    assert len(data) == 4
+
+
+def test_get_formatted_data(test_data):
+    data = get_formatted_data(test_data)
+    assert data == ['26.08.2019 Перевод организации\nMaestro 1596 83** **** 5 -> Счет **9589\n31957.58 руб.', '03.07.2020 Перевод организации\nMasterCard 7158 30** **** 6 -> Счет **5560\n8221.37 USD', '30.06.2018 Перевод организации\nСчет 7510 68** **** 6 -> Счет **6702\n9824.07 USD', '23.03.2018 Открытие вклада\n  -> Счет **2431\n48223.05 руб.', '04.04.2019 Перевод со счета на счет\nСчет 1970 86** **** 8 -> Счет **4188\n79114.93 USD']
+
